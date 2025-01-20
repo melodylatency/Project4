21078d0cc0228017753720b0dcf79f23b04d9f33
melodylatency <melodylatency@yahoo.com>
2025-01-20 19:06:20 +0500

    final

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,15 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorHandler.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
 connectDB();
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -20,11 +23,19 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API running...");
-});
-
 app.use("/api/users", userRoutes); // User express router, we are shortening the url
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
